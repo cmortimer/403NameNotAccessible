d600b34cc0cb944b4ce4632127725dd566a69df2
ZRBusch <zrb6785@NM2550-17.main.ad.rit.edu>
2015-09-14 15:58:14 -0400

    Implementing a basic menu
    Currently the build opens the menu, hit head out then destination 1 to
    go to asdfghjkl

---
+++ TheGameThatShallNotBeNamed/Assets/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public Button[] mainButtons = new Button[5];
+	public Button[] destinations = new Button[5];
+
+	//set correct menu options to active
+	void Start(){
+		foreach(Button b in destinations){
+			b.gameObject.SetActive (false);
+		}
+	}
+
+
+	//function handlers for each button
+	public void MainToDest(){
+		foreach(Button b in mainButtons){
+			b.gameObject.SetActive(false);
+		}
+		foreach(Button b2 in destinations){
+			b2.gameObject.SetActive (true);
+		}
+
+	}
+
+	public void DestToMain(){
+		foreach(Button b in destinations){
+			b.gameObject.SetActive(false);
+		}
+		foreach(Button b2 in mainButtons){
+			b2.gameObject.SetActive (true);
+		}
+		
+	}
+
+	public void VisitDest(int level){
+		Application.LoadLevel (1);
+	}
+}
