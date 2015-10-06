cb258b0c8ad6fc1c2a9ae7ca7ea1085f3bcb6c6b
ZRBusch <zrb6785@OH1375-9.main.ad.rit.edu>
2015-10-06 09:35:52 -0400

    Equipment start
    equipment script is just a class that holds all possible equipment
    pieces and structs for a single piece of armor or single weapon. Also
    the text file has comments as to how to add in new weapons/armor pieces.

---
+++ TheGameThatShallNotBeNamed/Assets/Scripts/Equipment.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+public class Equipment : MonoBehaviour{
+	//a single weapon
+	public struct weapon{
+		public string name;
+		public string desc;
+		public int str;
+		public int end ;
+		public int agi ;
+		public int mag ;
+		public int luck ;
+		public int rangeMin ;
+		public int rangeMax;
+		public List<string> recipe;
+		//something for an image to show
+	}
+
+	//a single piece of armor
+	public struct armor{
+		public string name;
+		public string desc;
+		public int str;
+		public int end ;
+		public int agi ;
+		public int mag ;
+		public int luck ;
+		public List<string> recipe;
+		//image?
+	}
+
+	//a list of every weapon that exists in the game...
+	public List<weapon> allWeapons = new List<weapon>();
+
+	//same thing for armor...
+	public List<armor> allArmor = new List<armor>();
+
+	bool LoadEquipment(string fileName){
+		try
+		{
+			string line;
+
+			StreamReader reader = new StreamReader(fileName, Encoding.Default);
+
+			using (reader)
+			{
+				do
+				{
+					line = reader.ReadLine();
+
+					if (line != null)
+					{
+						//skip comments
+						if(line[0] == 'c'){	continue; }
+						//split content lines
+						string[] entries = line.Split(',');
+						if(entries[0][0] == 'w'){
+							weapon temp = new weapon();
+							temp.name = entries[1];
+							temp.desc = entries[2];
+							temp.str = Int32.Parse(entries[3]);
+							temp.end = Int32.Parse(entries[4]);
+							temp.agi = Int32.Parse(entries[5]);
+							temp.mag = Int32.Parse(entries[6]);
+							temp.luck = Int32.Parse(entries[7]);
+							temp.rangeMin = Int32.Parse(entries[8]);
+							temp.rangeMax = Int32.Parse(entries[9]);
+							for(int i=10;i<entries.Length;i++){
+								temp.recipe.Add(entries[i]);
+							}
+							allWeapons.Add(temp);
+						}
+						if(entries[0][0] == 'a'){
+							armor temp = new armor();
+							temp.name = entries[1];
+							temp.desc = entries[2];
+							temp.str = Int32.Parse(entries[3]);
+							temp.end = Int32.Parse(entries[4]);
+							temp.agi = Int32.Parse(entries[5]);
+							temp.mag = Int32.Parse(entries[6]);
+							temp.luck = Int32.Parse(entries[7]);
+							for(int i=8;i<entries.Length;i++){
+								temp.recipe.Add(entries[i]);
+							}
+							allArmor.Add(temp);
+						}
+
+					}
+				}
+				while (line != null);
+
+				reader.Close();
+				return true;
+			}
+		}
+		catch(IOException e){
+			Debug.Log (e.Message);
+			return false;
+		}
+	}
+}
