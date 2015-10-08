cd5c79ded077531d0bd28745b78c2fd312004658
ZRBusch <zrb6785@OH1375-9.main.ad.rit.edu>
2015-10-08 09:16:50 -0400

    Equipment list and data
    persistent data and equipment lists started

--- TheGameThatShallNotBeNamed/Assets/Scripts/Equipment.cs
+++ TheGameThatShallNotBeNamed/Assets/Scripts/Equipment.cs
@@ -41,7 +41,8 @@ public class Equipment : MonoBehaviour{
 	//same thing for armor...
 	public List<armor> allArmor = new List<armor>();
 
-	bool LoadEquipment(string fileName){
+	public bool LoadEquipment(string fileName){
+
 		try
 		{
 			string line;
@@ -71,7 +72,9 @@ public class Equipment : MonoBehaviour{
 							temp.luck = Int32.Parse(entries[7]);
 							temp.rangeMin = Int32.Parse(entries[8]);
 							temp.rangeMax = Int32.Parse(entries[9]);
+							temp.recipe = new List<string>();
 							for(int i=10;i<entries.Length;i++){
+
 								temp.recipe.Add(entries[i]);
 							}
 							allWeapons.Add(temp);
@@ -85,6 +88,7 @@ public class Equipment : MonoBehaviour{
 							temp.agi = Int32.Parse(entries[5]);
 							temp.mag = Int32.Parse(entries[6]);
 							temp.luck = Int32.Parse(entries[7]);
+							temp.recipe = new List<string>();
 							for(int i=8;i<entries.Length;i++){
 								temp.recipe.Add(entries[i]);
 							}
@@ -96,6 +100,7 @@ public class Equipment : MonoBehaviour{
 				while (line != null);
 
 				reader.Close();
+				//Debug.Log(allArmor.Count);
 				return true;
 			}
 		}
---
+++ TheGameThatShallNotBeNamed/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+public class PlayerData : MonoBehaviour {
+	/*will contain, possibly a text file for save data
+	 *all enemy types and their stats/drops/where they spawn
+	 *all equipment; armor and weapons
+	 *list of all people in player's current party
+	 *current quests
+	*/
+
+	/*
+	 * everything for equipment
+	 */
+	//a single weapon
+	public struct weapon{
+		public string name;
+		public string desc;
+		public int str;
+		public int end ;
+		public int agi ;
+		public int mag ;
+		public int luck ;
+		public int rangeMin ;
+		public int rangeMax;
+		public List<string> recipe;
+		//something for an image to show
+	}
+	
+	//a single piece of armor
+	public struct armor{
+		public string name;
+		public string desc;
+		public int str;
+		public int end ;
+		public int agi ;
+		public int mag ;
+		public int luck ;
+		public List<string> recipe;
+		//image?
+	}
+	public Equipment equipment;
+	List<weapon> obtainedWeapons;
+	List<armor> obtainedArmor;
+
+	public static PlayerData Instance;
+	// Use this for initialization
+	void Awake () {
+		if(Instance)
+		{
+			DestroyImmediate(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+
+		equipment = this.gameObject.GetComponent<Equipment>();
+		equipment.LoadEquipment("Assets\\Scripts\\EquipmentList.txt");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
