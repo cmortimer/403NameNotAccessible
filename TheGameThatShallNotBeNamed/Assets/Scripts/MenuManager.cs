c2453ecaadc393546f640bca204bd3a3911d545c
ZRBusch <zrb6785@OH1375-9.main.ad.rit.edu>
2015-10-20 09:50:28 -0400

    Equipment menu
    list of items show up. Need to implement the button handler and
    scrolling up/down

--- TheGameThatShallNotBeNamed/Assets/Scripts/MenuManager.cs
+++ TheGameThatShallNotBeNamed/Assets/Scripts/MenuManager.cs
@@ -10,7 +10,8 @@ public class MenuManager : MonoBehaviour {
 	public GameObject workshopMenu;
 	public GameObject destMenu;
 	public GameObject tavernMenu;
-	
+	public GameObject equipmentPrefab;
+
 	Equipment equip;
 	
 	//set correct menu options to active
@@ -24,8 +25,39 @@ public class MenuManager : MonoBehaviour {
 	
 	//setup workshop
 	public void SetupWorkshop(){
+		int currentY = 200;
 		for(int i=0;i<equip.allWeapons.Count;i++){
-			//equip.allWeapons[i]
+			GameObject currentEquip = equipmentPrefab;
+			GameObject imgObj = currentEquip.transform.FindChild("Image").gameObject;
+			//set image = equipment image path
+			GameObject nameObj = currentEquip.transform.FindChild("NameDesc").gameObject;
+			nameObj.GetComponent<Text>().text = equip.allWeapons[i].name;
+			GameObject statsObj = currentEquip.transform.FindChild("Stats").gameObject;
+			string text = equip.allWeapons[i].str + "    " + equip.allWeapons[i].end + "     " + equip.allWeapons[i].agi + "     " + equip.allWeapons[i].mag + "     " + equip.allWeapons[i].luck + "    " + equip.allWeapons[i].rangeMin + " - " + equip.allWeapons[i].rangeMax;
+			statsObj.GetComponent<Text>().text = text;
+			GameObject recipeObj = currentEquip.transform.FindChild("Recipe").gameObject;
+			
+			GameObject createObj = currentEquip.transform.FindChild("Create").gameObject;
+			GameObject finished = (GameObject)Instantiate(currentEquip, new Vector3(workshopMenu.transform.position.x-150.0f,workshopMenu.transform.position.y+currentY,0.0f), Quaternion.identity);
+			finished.transform.parent = workshopMenu.transform;
+			currentY -= 75;
+		}
+		
+		for(int i=0;i<equip.allArmor.Count;i++){
+			GameObject currentEquip = equipmentPrefab;
+			GameObject imgObj = currentEquip.transform.FindChild("Image").gameObject;
+			//set image = equipment image path
+			GameObject nameObj = currentEquip.transform.FindChild("NameDesc").gameObject;
+			nameObj.GetComponent<Text>().text = equip.allArmor[i].name;
+			GameObject statsObj = currentEquip.transform.FindChild("Stats").gameObject;
+			string text = equip.allArmor[i].str + "    " + equip.allArmor[i].end + "     " + equip.allArmor[i].agi + "     " + equip.allArmor[i].mag + "     " + equip.allArmor[i].luck + "     " ;
+			statsObj.GetComponent<Text>().text = text;
+			GameObject recipeObj = currentEquip.transform.FindChild("Recipe").gameObject;
+			
+			GameObject createObj = currentEquip.transform.FindChild("Create").gameObject;
+			GameObject finished = (GameObject)Instantiate(currentEquip, new Vector3(workshopMenu.transform.position.x-150.0f,workshopMenu.transform.position.y+currentY,0.0f), Quaternion.identity);
+			finished.transform.parent = workshopMenu.transform;
+			currentY -= 75;
 		}
 	}
 	
@@ -34,27 +66,30 @@ public class MenuManager : MonoBehaviour {
 		mainMenu.SetActive(false);
 		destMenu.SetActive(true);
 	}
-	
 	public void DestToMain(){
 		destMenu.SetActive(false);
 		mainMenu.SetActive(true);
 	}
-	
 	public void MainToWorkshop(){
 		mainMenu.SetActive(false);
 		workshopMenu.SetActive(true);
 	}
-
 	public void MainToTavern(){
 		mainMenu.SetActive(false);
 		tavernMenu.SetActive(true);
 	}
-
 	public void TaverToMain(){
 		mainMenu.SetActive(true);
 		tavernMenu.SetActive(false);
 	}
-	
+	public void WorkToMain(){
+		workshopMenu.SetActive(false);
+		mainMenu.SetActive(true);
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+
 	public void VisitDest(int level){
 		Application.LoadLevel (2);
 	}
